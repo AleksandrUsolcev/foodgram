f407b412052bd05dfb3c9f44adf9b41aa8ae048d
Aleksandr Usolcev <alexusolev@gmail.com>
2022-10-29 16:48:40 +0300

    recipes models, admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Tag, Ingredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('created', 'name', 'author')
+    list_editable = ('name',)
+    list_filter = ('author', 'name', 'tags')
+    ordering = ('created',)
+# На странице рецепта вывести общее число добавлений этого рецепта в избранное.
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit', 'count')
+    list_filter = ('name',)
+    ordering = ('id',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    ordering = ('id',)
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,89 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=254
+    )
+    color = models.CharField(
+        verbose_name='HEX-код',
+        max_length=7
+    )
+    slug = models.SlugField(
+        verbose_name='Slug'
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=254
+    )
+    count = models.PositiveIntegerField(
+        verbose_name='Количество'
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=64
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        related_name='recipes',
+        on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        related_name='recipes'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингредиенты',
+        related_name='recipes'
+    )
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='recipes/',
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время приготовления в минутах'
+    )
+    text = models.TextField(
+        verbose_name='Описание'
+    )
+    created = models.DateTimeField(
+        verbose_name='Дата добавления',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return f'{self.name}'
