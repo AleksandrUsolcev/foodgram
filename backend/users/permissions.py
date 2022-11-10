7f3919630d277d09e658048a5662609a67f96810
Aleksandr Usolcev <alexusolev@gmail.com>
2022-11-10 16:54:34 +0300

    fix permissions

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -16,11 +16,9 @@ class AllowAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        else:
-            return request.user.is_authenticated
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        else:
-            return obj.author == request.user
+        return obj.author == request.user
